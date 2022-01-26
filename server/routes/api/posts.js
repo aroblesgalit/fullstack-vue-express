ec80055bfd7718f6241d12066eaf469f03f60ec8
Alvin Galit <aroblesgalit@gmail.com>
2022-01-25 21:54:10 -0700

    Created async method for loading post collection

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -4,12 +4,24 @@ const mongodb = require('mongodb')
 const router = express.Router()
 
 // Get Posts
-router.get('/', (req, res) => {
-  res.send('hello')
+router.get('/', async (req, res) => {
+  const posts = await loadPostCollection()
+  res.send(await posts.find({}).toArray())
 })
 
 // Add Post
 
 // Delete Post
+
+async function loadPostCollection () {
+  const client = await mongodb.MongoClient.connect(
+    'mongodb+srv://aroblesgalit:jOtjYrtOdKFCMm2n@vue-express.r7fqb.mongodb.net/vue-express?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true
+    }
+  ) // Connects to database
+
+  return client.db('vue-express').collection('posts')
+}
 
 module.exports = router
